d78adb082ec7d345aeea24cbb8193b2326ad4a4c
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2023-09-26 13:52:00 -0400

    Began styling cards on Deck.tsx

--- CapstoneFlashcardApplication/src/pages/Deck.tsx
+++ CapstoneFlashcardApplication/src/pages/Deck.tsx
@@ -1,3 +1,4 @@
+// cardData is imported to utilize the items in the array
 import cardData from '../data/cardData'
 import Card from '../components/Card';
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -9,9 +10,17 @@ const Deck = () => {
     // test if the function in component is executed 
     console.log("INSIDE OF Deck, does it get here ?!?!?!?!?!    ")
 
-    const cardList = cardData.map(card => 
+    // iterate through the array;
+    // map is a commonly used JS/TS array function
+    // that takes each entry in the array and does something with it.
+    // In this case, it 'maps' it with a TSX element
+    const cardList = cardData.map(card =>
         <>
-            <Card key={card.id} card={card}/>
+            <div className="cardListItem" key={card.id} onClick={() => console.log("Card clicked")}>
+                <h3 className='side1'>{card.side1}</h3>
+                <h3 className='side2'>{card.side2}</h3>
+                <h3 className='pronunciation'>{card.pronunciation}</h3>
+            </div>
         </>
     )
 
