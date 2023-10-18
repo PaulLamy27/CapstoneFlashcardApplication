1507dc07c1bdc70427fa1518dc152e5b28a9a94b
Sisi-C <sirisa.chueainta@snhu.edu>
2023-10-17 21:30:37 -0400

    Deck using tailwind

--- CapstoneFlashcardApplication/src/pages/Deck.tsx
+++ CapstoneFlashcardApplication/src/pages/Deck.tsx
@@ -3,7 +3,6 @@ import cardData from '../data/cardData'
 import Card from '../components/Card';
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './Deck.css'
 import { useState } from 'react';
 
 const Deck = () => {
@@ -27,36 +26,36 @@ const Deck = () => {
     // In this case, it 'maps' it with a TSX element
     let cardList = cards.map(card =>
         <>
-            <div className="cardListItem" key={card.id} onClick={() => console.log("Card clicked")}>
-                <div className="contentContainer">
-                    <h1 className='side1'>{card.side1}</h1>
-                    <h1 className='side2'>{card.side2}</h1>
-                    <h1 className='pronunciation'>{card.pronunciation}</h1>
-                </div>
+            <div key={card.id} className='bg-gray-50 m-5 p-5 w-60 h-50 rounded-lg text-black text-center'>
+                <h1>{card.side1}</h1>
+                <h2>{card.side2}</h2>
+                <h2>{card.pronunciation}</h2>
             </div>
         </>
     )
 
     return (
         <>
-            <div className="mainDeckContainer">
-                <h1 className='deckName'>Name of Deck</h1>
-                <div>
-                    <div>
+            <div className="flex items-center justify-center w-800 text-white">
+                <div className="flex flex-col justify-center p-10 m-10">
+                    <h1>Name of Deck</h1>
+                    <div className="w-full mx-auto">
                         <input value={frontSide}
                             onChange={e => setFrontSide(e.target.value)} />
                         <input value={backSide}
                             onChange={e => setbackSide(e.target.value)} />
                         <input value={pronounced}
                             onChange={e => setPronounced(e.target.value)} />
-                        <button onClick={() => {
+                        <button className="border rounded-lg m-5 p-2 bg-[#00df9a] hover:bg-[#4DE3B5] text-[#13163b] font-medium" onClick={() => {
                             setCards([
                                 ...cards,
                                 {id: nextId++, side1: frontSide,  side2: backSide, pronunciation: pronounced}
                             ]);
                         }}>CLICK TO ADD CARD</button>
                     </div>
-                    {cardList}
+                    <div className="grid grid-cols-3 p-5">
+                        {...cardList}
+                    </div>
                 </div>
             </div>
         </>
