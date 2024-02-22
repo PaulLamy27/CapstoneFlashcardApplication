fa8d691d794b1a6fad63bac6c11653676a1784d7
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-02-22 10:28:28 -0500

    public-private works with new deck

--- CapstoneFlashcardApplication/src/pages/YourDecks.tsx
+++ CapstoneFlashcardApplication/src/pages/YourDecks.tsx
@@ -95,21 +95,11 @@ const YourDecks = () => {
 
     // upon first load
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/deck/user/`, { withCredentials: true })
-            .then((res) => {
-                console.log(res.data);
-                const titles = res.data.map((deck: { title: String; }) => deck.title);
-                setDeckList(titles);
-                setIsPublicList(res.data.map(deck => deck.isPublic));
-            })
-            .catch((error) => {
-                console.log("The following error occured in axios.get: ", error);
-            });
         fetchData();
     }, []);
 
     useEffect(() => {
-        
+
     }, [deckList]);
 
     const toggleAddCardComponent = () => {
@@ -118,39 +108,6 @@ const YourDecks = () => {
 
     return (
         <>
-        <div className='flex flex-col text-black'>
-            <div className='flex items-center justify-center w-800 font-sans text-white font-semibold text-3xl'>Your Decks</div>
-            <div className="h-screen mx-8 grid grid-cols-3 p-5">
-                {deckList.map((deckName, index) => (
-                    <ul className='flex relative group cursor-pointer justify-center items-center text-center max-w-[300px] h-40 mb-20 bg-slate-50 text-black font-semibold text-xl transition-opacity duration-300 ease-in-out hover:bg-slate-200 hover:opacity-80' key={index}>
-                        {
-                        showConfirmationIndex == index ?
-                        (
-                            <ConfirmationDialog
-                            message="Are you sure you want to delete this deck?"
-                            onConfirm={() => deleteDeck(deckName)}
-                            onCancel={() => setShowConfirmationIndex(null)}/> 
-                        )
-                        :
-                        (
-                            // link to is the URL that leads to that page
-                            <>
-                            {isPublicList[index] == 1 ? 
-                            (<><MdPublic className='absolute top-0 right-8 hidden group-hover:block w-8 h-8 cursor-pointer hover:text-gray-400' onClick={() => updateDeck(index, deckName, 0)}/></>):
-                            (<><MdPublicOff className='absolute top-0 right-8 hidden group-hover:block w-8 h-8 cursor-pointer hover:text-gray-400' onClick={() => updateDeck(index, deckName, 1)}/></>)}
-                            <MdDelete className='absolute top-0 right-0 hidden group-hover:block w-8 h-8 cursor-pointer text-red-500 hover:text-red-400' onClick={() => openConfirmationDialog(index)} />
-                            <Link className="p-20" to={`/your-decks/${deckName}`} key={index}>
-                                <p>{deckName}</p>
-                            </Link>
-                            </>
-                        )}
-                    </ul>
-                ))}
-    
-                <div className=''>
-                    {showAddCardComponent && <AddCard onClose={toggleAddCardComponent} />}
-                </div>
-            </div >
             <div className='flex flex-col text-black'>
                 <div className='flex items-center justify-center w-800 font-sans text-white font-semibold text-3xl'>Your Decks</div>
                 <div className="h-screen mx-8 grid grid-cols-3 p-5">
@@ -168,6 +125,9 @@ const YourDecks = () => {
                                     (
                                         // link to is the URL that leads to that page
                                         <>
+                                            {isPublicList[index] == 1 ?
+                                                (<><MdPublic className='absolute top-0 right-8 hidden group-hover:block w-8 h-8 cursor-pointer hover:text-gray-400' onClick={() => updateDeck(index, deckName, 0)} /></>) :
+                                                (<><MdPublicOff className='absolute top-0 right-8 hidden group-hover:block w-8 h-8 cursor-pointer hover:text-gray-400' onClick={() => updateDeck(index, deckName, 1)} /></>)}
                                             <MdDelete className='absolute top-0 right-0 hidden group-hover:block w-8 h-8 cursor-pointer text-red-500 hover:text-red-400' onClick={() => openConfirmationDialog(index)} />
                                             <Link className="p-20" to={`/your-decks/${deckName}`} key={index}>
                                                 <p>{deckName}</p>
@@ -191,8 +151,8 @@ const YourDecks = () => {
                             </div>
                         }
                     </div>
-                </div >
 
+                </div >
 
                 <div className='fixed bottom-4 left-4' onClick={() => setShowAddCardComponent(true)}>
                     <div className='flex items-center justify-center w-12 h-12 text-[#00df9a] bg-transparent rounded-full cursor-pointer'>
