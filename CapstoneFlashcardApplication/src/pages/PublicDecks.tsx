4d3fcd51a55dd242f04b4b8aee1eff3d0e5e8f13
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-04-09 13:56:57 -0400

    vercel build issues 50 percent complete

--- CapstoneFlashcardApplication/src/pages/PublicDecks.tsx
+++ CapstoneFlashcardApplication/src/pages/PublicDecks.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const PublicDecks = () => {
 
