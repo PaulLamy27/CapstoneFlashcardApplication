d54843e45f49cc2c1222706690479b594a0b9581
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-04-23 09:37:11 -0400

    registration axiosInst no build error

--- CapstoneFlashcardApplication/src/pages/Registration.tsx
+++ CapstoneFlashcardApplication/src/pages/Registration.tsx
@@ -1,6 +1,5 @@
 import { useState} from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
 import axiosInstance from "../axiosInstance";
 
 const Registration = () => { 
