b318e3f13bbb2b997df59900a2fc7cb507a1ffe6
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-08-21 16:35:21 -0400

    testing locally; check Navbar, ensure works

--- CapstoneFlashcardApplication/src/axiosInstance.ts
+++ CapstoneFlashcardApplication/src/axiosInstance.ts
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: 'https://capstone-flashcard-api.vercel.app'
+    // baseURL: 'http://localhost:5000'
 });
 
 export default axiosInstance;
--- CapstoneFlashcardApplication/src/pages/Navbar.tsx
+++ CapstoneFlashcardApplication/src/pages/Navbar.tsx
@@ -23,129 +23,194 @@ const Navbar = () => {
     try {
       if (username) {
         setAuth(true);
-        if (auth) {
-          console.log("auth", auth);
-        }
         setName(username);
-        // Set the token as a common header for all requests
-        // axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
-
-        // const arrayToken = userToken.split('.');
-        // console.log("arrayToken", arrayToken);
-        // const tokenPayload = JSON.parse(atob(arrayToken[1]));
-        // console.log("tokenPayload", tokenPayload);
-
-        // setAuth(true);
-        // sessionStorage.setItem("username", tokenPayload.username);
-        // const username = sessionStorage.getItem('username');
-        // setName(username);
-        // sessionStorage.setItem("id", tokenPayload.id);
-        // const id = sessionStorage.getItem('id');
-        // console.log("id ", id);
-
-
-        // Make the request to '/'
-        // axiosInstance.get('/')
-        //   .then(res => {
-        //     if (res.data.Status === "Success") {
-        //       setAuth(true)
-        //       setName(res.data.username)
-        //       console.log("Username ", res.data.username)
-        //     } else {
-        //       setAuth(false)
-        //       setMessage(res.data.Error)
-        //       console.log("Username NOT set")
-        //     }
-        //   })
-        //   .catch(err => console.log(err));
+      } else {
+        setAuth(false);
+        setName('');
       }
+
+      // if (auth) {
+      //   console.log("auth", auth);
+      // }
+      // setName(username);
+
+
+      // Set the token as a common header for all requests
+      // axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
+
+      // const arrayToken = userToken.split('.');
+      // console.log("arrayToken", arrayToken);
+      // const tokenPayload = JSON.parse(atob(arrayToken[1]));
+      // console.log("tokenPayload", tokenPayload);
+
+      // setAuth(true);
+      // sessionStorage.setItem("username", tokenPayload.username);
+      // const username = sessionStorage.getItem('username');
+      // setName(username);
+      // sessionStorage.setItem("id", tokenPayload.id);
+      // const id = sessionStorage.getItem('id');
+      // console.log("id ", id);
+
+
+      // Make the request to '/'
+      // axiosInstance.get('/')
+      //   .then(res => {
+      //     if (res.data.Status === "Success") {
+      //       setAuth(true)
+      //       setName(res.data.username)
+      //       console.log("Username ", res.data.username)
+      //     } else {
+      //       setAuth(false)
+      //       setMessage(res.data.Error)
+      //       console.log("Username NOT set")
+      //     }
+      //   })
+      //   .catch(err => console.log(err));
     } catch (error) {
-      console.log("error in useEffect: ", error);
-      setAuth(false);
-      // setMessage("Not Authenticated");
-    }
+    console.log("error in useEffect: ", error);
+    setAuth(false);
+    // setMessage("Not Authenticated");
+  }
 
 
-    // if (userToken) {
-    //   // Set the token as a common header for all requests
-    //   axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
 
-    //   // get '/' => route at 'api/' => verifyUser 
-    //   axiosInstance.get('/')
-    //     // axios.get('http://localhost:5000/')
-    //     .then(res => {
-    //       if (res.data.Status === "Success") {
-    //         setAuth(true)
-    //         setName(res.data.username)
-    //         console.log("Username:", res.data.username);
-    //       }
-    //       else {
-    //         setAuth(false)
-    //         console.log("error with navbar: ", message);
-    //         setMessage(res.data.Error)
-    //       }
-    //     })
-    //     .catch(err => console.log(err));
-    // }
+  // if (userToken) {
+  //   // Set the token as a common header for all requests
+  //   axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
 
-  }, [])
+  //   // get '/' => route at 'api/' => verifyUser 
+  //   axiosInstance.get('/')
+  //     // axios.get('http://localhost:5000/')
+  //     .then(res => {
+  //       if (res.data.Status === "Success") {
+  //         setAuth(true)
+  //         setName(res.data.username)
+  //         console.log("Username:", res.data.username);
+  //       }
+  //       else {
+  //         setAuth(false)
+  //         console.log("error with navbar: ", message);
+  //         setMessage(res.data.Error)
+  //       }
+  //     })
+  //     .catch(err => console.log(err));
+  // }
+
+}, [])
 
   useEffect(() => {
     // Close the navbar when the location changes
     setNav(false);
   }, [location]);
 
-  const handleNav = () => {
-    setNav(!nav);
-  };
+const handleNav = () => {
+  setNav(!nav);
+};
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        // Show the scroll-to-top button when the user scrolls past a certain threshold (adjust as needed)
-        setShowScrollToTop(true);
-      } else {
-        setShowScrollToTop(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const refCallback = (node) => {
-    if (node) {
-      document.querySelectorAll("ul > li >  a").forEach(i => i.className = "hover:bg-skin-button p-1");
+useEffect(() => {
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      // Show the scroll-to-top button when the user scrolls past a certain threshold (adjust as needed)
+      setShowScrollToTop(true);
+    } else {
+      setShowScrollToTop(false);
     }
-  }
+  };
 
-  return (
-    <div ref={refCallback} className="flex justify-between item-center h-24 max-w-full mx-auto px-4 text-skin-base">
-      <Link to="/">
-        <div className="flex">
-          <svg width="75" height="75">
-            <image href={logo} height="100%" width="100%" />
-          </svg>
-          <p className="text-3xl font-bold  text-skin-header">
-            CARDMENTOR.
-          </p>
-        </div>
-      </Link>
-      <ul className="hidden md:flex">
-        <li className="p-4">
+  window.addEventListener("scroll", handleScroll);
+
+  return () => {
+    window.removeEventListener("scroll", handleScroll);
+  };
+}, []);
+
+const refCallback = (node) => {
+  if (node) {
+    document.querySelectorAll("ul > li >  a").forEach(i => i.className = "hover:bg-skin-button p-1");
+  }
+}
+
+return (
+  <div ref={refCallback} className="flex justify-between item-center h-24 max-w-full mx-auto px-4 text-skin-base">
+    <Link to="/">
+      <div className="flex">
+        <svg width="75" height="75">
+          <image href={logo} height="100%" width="100%" />
+        </svg>
+        <p className="text-3xl font-bold  text-skin-header">
+          CARDMENTOR.
+        </p>
+      </div>
+    </Link>
+    <ul className="hidden md:flex">
+      <li className="p-4">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="p-4">
+        {
+          auth ?
+            <Link to="/your-decks">Decks</Link>
+            :
+            <Link to="/login">Decks</Link>
+        }
+      </li>
+      <li className="p-4">
+        {
+          auth ?
+            <Link to="/study">Study</Link>
+            :
+            <Link to="/login">Study</Link>
+        }
+      </li>
+      <li className="p-4">
+        {
+          auth ?
+            <Link to="/user">Search user</Link>
+            :
+            <Link to="/login">Search user</Link>
+        }
+      </li>
+      <li className="p-4">
+        {
+          auth ?
+            <Link to="/PublicDecks">Search Public Decks</Link>
+            :
+            <Link to="/login">Search user</Link>
+        }
+      </li>
+      {
+        auth ?
+          <li className="p-4">
+            <Link to={`/profile/${name}`}>{name}</Link>
+          </li>
+          :
+          <li className="p-4">
+            <Link to="/login">Login</Link>
+          </li>
+      }
+    </ul>
+    <div onClick={handleNav} className="block md:hidden mt-3">
+      {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+    </div>
+    <div
+      className={
+        nav
+          ? "fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-600 bg-skin-bg ease-in-out duration-500 z-50"
+          : "fixed left-[-100%]"
+      }
+    >
+      <ul className="uppercase">
+        <li className="p-4 border-b border-skin-dark">
           <Link to="/">Home</Link>
         </li>
-        <li className="p-4">
+        <li className="p-4 border-b border-skin-dark">
           {
             auth ?
               <Link to="/your-decks">Decks</Link>
@@ -153,7 +218,7 @@ const Navbar = () => {
               <Link to="/login">Decks</Link>
           }
         </li>
-        <li className="p-4">
+        <li className="p-4 border-b border-skin-dark">
           {
             auth ?
               <Link to="/study">Study</Link>
@@ -161,7 +226,7 @@ const Navbar = () => {
               <Link to="/login">Study</Link>
           }
         </li>
-        <li className="p-4">
+        <li className="p-4 border-b border-skin-dark">
           {
             auth ?
               <Link to="/user">Search user</Link>
@@ -169,7 +234,7 @@ const Navbar = () => {
               <Link to="/login">Search user</Link>
           }
         </li>
-        <li className="p-4">
+        <li className="p-4 border-b border-skin-dark">
           {
             auth ?
               <Link to="/PublicDecks">Search Public Decks</Link>
@@ -188,73 +253,16 @@ const Navbar = () => {
             </li>
         }
       </ul>
-      <div onClick={handleNav} className="block md:hidden mt-3">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-      <div
-        className={
-          nav
-            ? "fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-600 bg-skin-bg ease-in-out duration-500 z-50"
-            : "fixed left-[-100%]"
-        }
-      >
-        <ul className="uppercase">
-          <li className="p-4 border-b border-skin-dark">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="p-4 border-b border-skin-dark">
-            {
-              auth ?
-                <Link to="/your-decks">Decks</Link>
-                :
-                <Link to="/login">Decks</Link>
-            }
-          </li>
-          <li className="p-4 border-b border-skin-dark">
-            {
-              auth ?
-                <Link to="/study">Study</Link>
-                :
-                <Link to="/login">Study</Link>
-            }
-          </li>
-          <li className="p-4 border-b border-skin-dark">
-            {
-              auth ?
-                <Link to="/user">Search user</Link>
-                :
-                <Link to="/login">Search user</Link>
-            }
-          </li>
-          <li className="p-4 border-b border-skin-dark">
-            {
-              auth ?
-                <Link to="/PublicDecks">Search Public Decks</Link>
-                :
-                <Link to="/login">Search user</Link>
-            }
-          </li>
-          {
-            auth ?
-              <li className="p-4">
-                <Link to={`/profile/${name}`}>{name}</Link>
-              </li>
-              :
-              <li className="p-4">
-                <Link to="/login">Login</Link>
-              </li>
-          }
-        </ul>
-      </div>
-      <FaAngleUp
-        size={40}
-        className={
-          showScrollToTop ? "fixed bottom-4 right-4 text-skin-header" : "hidden"
-        }
-        onClick={scrollToTop}
-      />
     </div>
-  );
+    <FaAngleUp
+      size={40}
+      className={
+        showScrollToTop ? "fixed bottom-4 right-4 text-skin-header" : "hidden"
+      }
+      onClick={scrollToTop}
+    />
+  </div>
+);
 };
 
 export default Navbar;
